9daab283b7667457ef44fb2d11df8197d1d9f2f2
Bittu kushwaha <bittukushwaha118@gmail.com>
2025-04-27 20:06:04 +0530

    Add files via upload

---
+++ ARRAY/Arr11repeatadddigiteff.java
@@ -0,0 +1,13 @@
+public class Arr11repeatadddigiteff {
+    static int singleDigit(int n) {
+        if (n == 0) 
+            return 0;
+        if(n % 9 == 0)
+            return 9;
+        return (n % 9);
+    }
+    public static void main(String args[]) {
+        int n = 1234;
+        System.out.println(singleDigit(n));
+    }
+}
