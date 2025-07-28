37db0fa01d478380bf20d426d5fbf88c35e231c8
Bittu kushwaha <bittukushwaha118@gmail.com>
2025-07-28 08:35:00 +0530

    Add files via upload

---
+++ ARRAY/Arr76subarrays.java
@@ -0,0 +1,17 @@
+public class Arr76subarrays {
+    public static void subarrays(int numbers[]){
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i;j<numbers.length;j++){
+            for(int k=i;k<=j;k++){
+              System.out.print(numbers[k]+" ");  
+            }
+            System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int numbers[]={2,4,6,8,10};
+        subarrays(numbers);
+    }
+}
