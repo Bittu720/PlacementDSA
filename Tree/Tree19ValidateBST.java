2e55d6ea68cbcb172b48936991c8267d7ccae4d4
Bittu kushwaha <bittukushwaha118@gmail.com>
2026-02-04 20:55:54 +0530

    Add files via upload

---
+++ Tree/Tree19ValidateBST.java
@@ -0,0 +1,66 @@
+public class Tree19ValidateBST {
+ static class Node{
+   int data;
+   Node left;
+   Node right;
+
+    Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+   }
+
+   public static Node insert(Node root, int val) {
+        if (root == null) {
+            return new Node(val);
+        }
+
+        if (val < root.data) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+   public static boolean isValid(Node root,Node min,Node max){
+    if(root==null){
+        return true;
+    }
+    if(min!=null && root.data<=min.data){
+        return false;
+    }
+    else if(max!=null && root.data>=max.data){
+        return false;
+    }
+
+    return isValid(root.left,min,root)&& isValid(root.right,root,max);
+   } 
+  public static void main(String args[]){
+   int values[]={8,5,3,6,10,11,14};
+   Node root=null;
+
+   for(int i=0;i<values.length;i++){
+    root=insert(root,values[i]);
+   }
+
+   inorder(root);
+   System.out.println();
+
+   if(isValid(root,null,null)){
+    System.out.println("valid");
+   }
+    else {
+        System.out.println("not valid");
+    }
+  }  
+}
