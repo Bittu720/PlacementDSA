20a8b5272ee16219cd919aafad1fcc00a75d03d9
Bittu kushwaha <bittukushwaha118@gmail.com>
2026-03-07 10:03:15 +0530

    Add files via upload

---
+++ ArrayList/Arraylist4swap2num.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Arraylist4swap2num {
+    public static void swap(ArrayList<Integer> list,int idx1,int idx2){
+       int temp=list.get(idx1);
+        list.set(idx1,list.get(idx2));
+        list.set(idx2,temp);
+    }
+    public static void main(String args[]){
+        ArrayList<Integer> list =new ArrayList<>();
+        list.add(2);
+        list.add(4);
+        list.add(6);
+        list.add(8);
+        list.add(10);
+        int idx1=1,idx2=3;
+        System.out.println(list);
+        swap(list,idx1,idx2);
+        System.out.println(list);
+    }
+}
